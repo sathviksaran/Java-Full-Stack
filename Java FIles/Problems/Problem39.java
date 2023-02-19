f6f9e433a868017a96b2215c7cd37e8644be11dc
Sathvik Saran Atchukolu <94753192+sathviksaran@users.noreply.github.com>
2023-02-19 09:37:14 +0530

    commit this

--- Java FIles/Problems/Problem39.java
+++ Java FIles/Problems/Problem39.java
@@ -1,35 +1,17 @@
-import java.util.*;
+import java.util.HashSet;
+
 public class Problem39 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        String n=sc.next();
-        ArrayList<Character> ch=new ArrayList<>();
-        if(n.length()==12)
-        {
-            int i=1,n1;
-            ch.add(n.charAt(0));
-            if(n.charAt(11)==n.charAt(10))
-            {
-                n1=11;
-            }
-            else{
-                n1=12;
-            }
-            while(ch.size()!=n1)
-            {
-                if(n.charAt(i)==n.charAt(i-1))
-                {
-                    char temp=n.charAt(i);
-                    ch.add(n.charAt(i+1));
-                    i+=2;
-                    ch.add(temp);
-                }
-                else{
-                    ch.add(n.charAt(i));
-                    i+=1;
-                }
-            }
-            System.out.println(ch);
-        }
+        HashSet<String> set=new HashSet<>();
+        HashSet<String> set1=new HashSet<>();
+        set.add("C");
+        set.add("Python");
+        set.add("Java");
+        System.out.println(set);
+        set1.addAll(set);
+        System.out.println(set1);
+        set.remove("Java");
+        System.out.println(set);
+        System.out.println(set1);
     }
 }
--- Java FIles/Problems/Problem40.java
+++ Java FIles/Problems/Problem40.java
@@ -1,17 +1,25 @@
-import java.util.HashSet;
-
-public class Problem40 {
+import java.util.*;
+public class Problem40{
     public static void main(String[] args) {
-        HashSet<String> set=new HashSet<>();
-        HashSet<String> set1=new HashSet<>();
-        set.add("C");
-        set.add("Python");
-        set.add("Java");
-        System.out.println(set);
-        set1.addAll(set);
-        System.out.println(set1);
-        set.remove("Java");
-        System.out.println(set);
-        System.out.println(set1);
+        Scanner sc=new Scanner(System.in);
+        HashMap<String,Integer> map=new HashMap<>();
+        String s="";
+        while(!s.equalsIgnoreCase("end"))
+        {
+            s=sc.next();
+            if(!s.equalsIgnoreCase("end"))
+            {
+                int n=sc.nextInt();
+                if(map.containsKey(s))
+                {
+                    map.put(s,map.get(s)+n);
+                }
+                else
+                {
+                    map.put(s,n);
+                }
+            }
+        }
+        System.out.println(map);
     }
 }
