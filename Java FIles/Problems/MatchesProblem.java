1933c46cc9ee33050badbbc4004ce1c7a62f6a50
Sathvik Saran Atchukolu <94753192+sathviksaran@users.noreply.github.com>
2023-02-20 17:54:13 +0530

    Update MatchesProblem.java

--- Java FIles/Problems/MatchesProblem.java
+++ Java FIles/Problems/MatchesProblem.java
@@ -2,40 +2,34 @@ import java.util.*;
 public class MatchesProblem {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        System.out.print("Enter no.of teams: ");
+        int n=sc.nextInt();
         ArrayList<String>teams=new ArrayList<>();
         ArrayList<Object>obj=new ArrayList<>();
-        ArrayList<HashMap<String,String>>hs=new ArrayList<>();
-        teams.add("team1");
-        teams.add("team2");
-        teams.add("team3");
-        teams.add("team4");
-        teams.add("team5");
-        match m1=new match(teams.get(0));
-        match m2=new match(teams.get(1));
-        match m3=new match(teams.get(2));
-        match m4=new match(teams.get(3));
-        match m5=new match(teams.get(4));
-        obj.add(m1);
-        obj.add(m2);
-        obj.add(m3);
-        obj.add(m4);
-        obj.add(m5);
-        new match(obj);
+        System.out.println("Enter Team Names: ");
+        for(int i=0;i<n;i++)
+        {
+            teams.add(sc.next());
+            Match m=new Match(teams.get(i));
+            obj.add(m);
+        }
+        new Match(obj);
     }
 }
-    class match
+class Match
+{
+    String s;
+    Match(String s)
     {
-        String s;
-        match(String s)
-        {
-            this.s=s;
-        }
-        public String toString()
-        {
-            return s;
-        }
-        match(ArrayList<Object> obj)
-        {
+        this.s=s;
+    }
+    public String toString()
+    {
+        return s;
+    }
+    Match(ArrayList<Object> obj)
+    {
+        System.out.println("Match Schedule: ");
         for(int i=0;i<=obj.size()-1;i++)
         {
             for(int j=0;j<obj.size()/2;j++)
